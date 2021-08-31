f8cc37c6587223d63336e27cfb829bf69223ed6f
JSheleg <jessica.sheleg@gmail.com>
2021-08-31 13:28:53 -0500

    create Comment schema and associate the pizza and comment models

--- models/Pizza.js
+++ models/Pizza.js
@@ -5,23 +5,47 @@ const { Schema, model } = require('mongoose');
 
 
 //schema for model
-const PizzaSchema = new Schema({
-    pizzaName: {
-        type: String
+const PizzaSchema = new Schema(
+    {
+        pizzaName: {
+            type: String
+        },
+        createdBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        size: {
+            type: String,
+            default: 'Large'
+        },
+        toppings: [], //indicates an array as data type
+        comments: [
+            {
+                //tell mongoose to expect ObjectId and that the data comes from Comment Model.
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
     },
-    createdBy: {
-        type: String
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-    size: {
-        type: String,
-        default: 'Large'
-    },
-    toppings: [] //indicates an array as data type
-});
+    {
+        //inform schema it can use virtuals(true)
+        // id to false as this is a virtual and we don't need it
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+//virtual property
+//care only about comment count in respect to pizzas
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
+  });
 
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 //file packages up all the models
 
 const Pizza = require('./Pizza');
+const Comment = require('./Comment');
 
-module.exports = { Pizza };
+module.exports = { Pizza, Comment };
